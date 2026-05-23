1de8fd240131d7fc577740a53d81209cacfb564c
dnhatdz <vawnhat18th6@gmail.com>
2026-05-23 12:40:01 +0700

    Update skibidimasterv2.lua

--- skibidimasterv2.lua
+++ skibidimasterv2.lua
@@ -315,18 +315,25 @@ task.spawn(function()
         end
     end
 end)
-
 -- ==========================================
--- 4. AUTO REJOIN SERVER (Sau 7 phút = 420 giây)
+-- AUTO RETRY (Sau 7 phút = 420 giây)
 -- ==========================================
-task.delay(420, function()
-    if #Players:GetPlayers() <= 1 then
-        pcall(function() TeleportService:Teleport(game.PlaceId, LocalPlayer) end)
-    else
-        pcall(function() TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, LocalPlayer) end)
+task.delay(410, function()
+    local success = false
+    for i = 1, 5 do
+        success = pcall(function()
+            game:GetService("ReplicatedStorage")
+                .Remotes
+                .client_server
+                .teleport_replay
+                :InvokeServer()
+        end)
+        if success then
+            break
+        end
+        task.wait(2)
     end
 end)
-
 -- ==========================================
 -- 5. NÚT BẬT/TẮT MENU (DÀNH CHO PC & MOBILE DELTA)
 -- ==========================================
